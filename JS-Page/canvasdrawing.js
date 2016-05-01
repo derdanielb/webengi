8e85ffbd8f8e3815f487ff1bf498eb8179f5a3e7
derdanielb <d.brilz@gmx.net>
2016-05-01 20:42:41 +0200

    page 2 arc content

--- JS-Page/canvasdrawing.js
+++ JS-Page/canvasdrawing.js
@@ -10,8 +10,8 @@ function drawLine(ctx, x, y, color) {
     ctx.lineWidth = 3;
     ctx.lineCap = "round";
     ctx.beginPath();
-    ctx.moveTo(oneLastX,oneLastY);
-    ctx.lineTo(x,y);
+    ctx.moveTo(oneLastX, oneLastY);
+    ctx.lineTo(x, y);
     ctx.stroke();
     oneLastX = x;
     oneLastY = y;
@@ -21,8 +21,9 @@ window.onload = function () {
     //setInterval(function () {
     //    catchClickRect(document.getElementById("drawCanvasOne"),null)
     //}, 3000);
-    setInterval(catchClickRect,2000,document.getElementById("drawCanvasOne"),null);
-    setInterval(catchClickLine,2000,document.getElementById("drawCanvasTwo"),null);
+    setInterval(catchClickRect, 2000, document.getElementById("drawCanvasOne"), null);
+    setInterval(catchClickLine, 2000, document.getElementById("drawCanvasTwo"), null);
+    setInterval(rectAutomation, 10000);
 
 };
 var oneLastX = 0;
@@ -31,7 +32,7 @@ var metroColors = ["#99b433", "#00a300", "#1e7145", "#ff0097", "#9f00a7", "#7e38
 function catchClickRect(context, event) {
     var canvas = document.getElementById(context.id);
     var ctx = canvas.getContext("2d");
-    var color = metroColors[Math.floor((Math.random() * 15) )];
+    var color = metroColors[Math.floor((Math.random() * 15))];
     var x, y;
     if (event) {
         x = (event.pageX - $("#" + context.id).offset().left);
@@ -46,7 +47,7 @@ function catchClickRect(context, event) {
 function catchClickLine(context, event) {
     var canvas = document.getElementById(context.id);
     var ctx = canvas.getContext("2d");
-    var color = metroColors[Math.floor((Math.random() * 16) )];
+    var color = metroColors[Math.floor((Math.random() * 16))];
     var x, y;
     if (event) {
         x = (event.pageX - $("#" + context.id).offset().left);
@@ -57,4 +58,35 @@ function catchClickLine(context, event) {
         y = Math.floor((Math.random() * 300) + 1);
     }
     drawLine(ctx, x, y, color);
+}
+function rectAutomation() {
+    console.log("asdf");
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 3; j++) {
+            setTimeout(drawz(i,j), 3000);
+        }
+    }
+
+}
+function drawz(i, j) {
+    console.log("@@@@@");
+    var canvas = document.getElementById('drawCanvasThree');
+
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    var x = 25 + j * 50; // x coordinate
+    var y = 25 + i * 50; // y coordinate
+    var radius = 20; // Arc radius
+    var startAngle = 0; // Starting point on circle
+    var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
+    var anticlockwise = i % 2 == 0 ? false : true; // clockwise or anticlockwise
+
+    ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+
+    if (i > 1) {
+        ctx.fill();
+    } else {
+        ctx.stroke();
+
+    }
 }
