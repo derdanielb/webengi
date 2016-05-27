e68bbf7cea92139b88111facf28c5769e1e483f5
derdanielb <d.brilz@gmx.net>
2016-05-27 16:43:48 +0200

    added folding added folded image

--- ccm_tree/ccm.brlz_tree.js
+++ ccm_tree/ccm.brlz_tree.js
@@ -11,15 +11,15 @@ ccm.component({
                 "tag": "div",
                 "class": "folder",
                 "id": "%placeholder%",
-                "droppable" : "true",
-                "ondrop":"drop(event)",
-                "ondragover":"allowDrop(event)",
+                "style" : "cursor: pointer;",
                 "inner": "a folder"
+
             };
             var fileref = {
                 "tag": "a",
                 "href": "",
                 "target": "brlz_iframe",
+                "style" : "text-decoration: none",
                 "inner": ""
             };
             var file = {
@@ -28,17 +28,16 @@ ccm.component({
                 "id": "%placeholder%"
             };
             var imageFolder = {
-              "tag":"img",
-                "src":"folder_blank_file16.png",
-                "align":"left",
+                "tag": "img",
+                "src": "folder_blank_file16.png",
+                "align": "left",
                 "style": "margin-right: 4px;"
             };
             var imageFile = {
-                "tag":"img",
-                "src":"new_document16.png",
-                "align":"left",
-                "draggable":"true",
-                "ondragstart":"drag(event)",
+                "tag": "img",
+                "src": "new_document16resized.png",
+                "align": "left",
+                "class": "FileImg",
                 "style": "margin-right: 4px;"
             };
             var element = ccm.helper.element(this);
@@ -51,6 +50,58 @@ ccm.component({
             function initTree() {
 
             }
+
+            function hideChildren(e) {
+                //console.log(e);
+                e.preventDefault();
+                e.stopPropagation();
+                console.log(element.find(this).attr("data-chidden"));
+
+                var children = element.find(this).children();
+
+                children.each(function (index, child) {
+                    if (index != 0) {
+                        element.find(child).toggle('fast');
+                    }
+                    else{
+                        if( element.find(child).attr('src') === "folder_blank_file16.png")
+                        {
+                            element.find(child).attr('src',"folder_blank_file16_folded.png");
+                        }else{
+                            element.find(child).attr('src',"folder_blank_file16.png");
+                        }
+                    }
+                });
+
+                //console.log(children);
+                //console.log(element.find(this).data("childrenhidden"));
+
+                //if(element.find(this).attr("data-chidden") == true)
+                //{
+                //    element.find(this).removeAttr("data-chidden");
+                //    element.find(this).attr("data-chidden", false);
+                //    //element.find(this).data("childrenhidden", "false");
+                //    children.each(function (index, child) {
+                //        //child.removeAttribute('hidden');
+                //        element.find(child).toggle show();
+                //    })
+                //}else
+                //{
+                //    element.find(this).removeAttr("data-chidden");
+                //    element.find(this).attr("data-chidden", true);
+                //    //element.find(this).data("childrenhidden", "true");
+                //    children.each(function (index, child) {
+                //        //console.log(child);
+                //        //child.setAttribute('hidden',"true");
+                //        element.find(child).hide();
+                //
+                //    })
+                //}
+                //element.find(this).unbind('click', hideChildren);
+                //element.find(this).on('click', hideChildren);
+
+            };
+
             function decideContent(parent, content) {
                 Object.getOwnPropertyNames(content).forEach(function (item) {
                     if (item === "Folder") {
@@ -61,6 +112,7 @@ ccm.component({
                     }
                 });
             }
+
             function displayFolder(parent, currentFolder) {
                 var foldercount = 0;
                 currentFolder.forEach(function (fldr) {
@@ -69,12 +121,34 @@ ccm.component({
                     Object.getOwnPropertyNames(fldr).forEach(function (node, ind, root) {
                         if (node === "name") {
                             newFolder.inner = fldr[node];
-                            newFolder.id = parent + "_"+foldercount;
+                            newFolder.id = parent + "_" + foldercount;
                             foldercount++;
                             var lvl = "20px";
-                            newFolder.style = "padding-left : "+ lvl;
+                            newFolder.style = "padding-left : " + lvl;
                             ccm.helper.find(that, '#' + parent).append(ccm.helper.html(newFolder));
                             ccm.helper.find(that, '#' + newFolder.id).append(ccm.helper.html(newImage));
+                            element.find('div#' + newFolder.id).on('click', hideChildren);
+                            //element.find('div#' + newFolder.id).on('click', function (e) {
+                            //    console.log(e);
+                            //    e.stopPropagation();
+                            //
+                            //    var children = element.find(this).children();
+                            //    if(element.find(this).data("childrenhidden"))
+                            //    {
+                            //        element.find(this).data("childrenhidden", "false");
+                            //        children.each(function (index, child) {
+                            //            child.hidden = false;
+                            //        })
+                            //    }else
+                            //    {
+                            //        element.find(this).data("childrenhidden", "true");
+                            //        children.each(function (index, child) {
+                            //            child.hidden = true;
+                            //        })
+                            //    }
+                            //
+                            //
+                            //})
                         }
                         if (node === "content") {
                             if (currentFolder[node] !== "") {
@@ -84,6 +158,7 @@ ccm.component({
                     })
                 })
             }
+
             function displayFile(parent, filesArray) {
                 var filecount = 0;
 
@@ -94,16 +169,33 @@ ccm.component({
                     newFile.id = parent + "-" + filecount;
                     newRef.href = file.ref;
                     newRef.inner = file.name;
+                    newRef.id = parent + "-" + filecount + "ref";
                     filecount++;
 
                     var lvl = "20px";
-                    newFile.style = "padding-left : "+ lvl;
+                    newFile.style = "padding-left : " + lvl;
 
 
                     ccm.helper.find(that, '#' + parent).append(ccm.helper.html(newFile));
                     var FolderElement = ccm.helper.find(that, '#' + newFile.id);
+                    //element.find('div#' + newFile.id).draggable( {
+                    //        containment: '#' + newFile.id,
+                    //        cursor: 'move'
+                    //}
+                    //);
+                    element.find('div#' + newFile.id).click(function (e) {
+                        e.stopPropagation();
+                    });
                     FolderElement.append(ccm.helper.html(newImage));
                     FolderElement.append(ccm.helper.html(newRef));
+                    //element.find('.FileImg').draggable({
+                    //
+                    //    cursor: 'move'
+                    //});
+                    //element.find('div#' + newFile.id+"ref").draggable( {
+                    //
+                    //    cursor: 'move'
+                    //});
                 })
             }
         }
