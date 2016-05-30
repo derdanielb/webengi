0247e0210d1bf2f8d2f59ec6504891881f6e6e6d
derdanielb <d.brilz@gmx.net>
2016-05-30 18:23:55 +0200

    clean up and some demos, explanation file

--- ccm_tree/ccm.brlz_tree.js
+++ ccm_tree/ccm.brlz_tree.js
@@ -44,29 +44,16 @@ ccm.component({
             };
             var veryLastFolder = false;
             var element = ccm.helper.element(this);
+            console.log(this);
             element.html(ccm.helper.html(that.html.get('root')));
             var structure;
-            $.getJSON('struct2.json', function (data) {
+            $.getJSON(this.dir, function (data) {
                 structure = data;
                 decideContent("node", data.Root, false);
-                //console.log(ccm.helper.find(that, '#node'));
-                //ccm.helper.find(that, '#node').children().each(function (index, child) {
-                //    console.log(child);
-                //    toogleHide(child.children());
-                //})
-
             });
-            function initTree() {
-
-            }
-
 
             function toogleHide(children) {
                 children.each(function (index, child) {
-                    //if (index != 0) {
-                    //    element.find(child).toggle('fast');
-                    //}
-                    //else{
                     if (element.find(child).attr('src') === "folder_blank_file16.png") {
                         element.find(child).attr('src', "folder_blank_file16_folded.png");
                     } else {
@@ -84,15 +71,11 @@ ccm.component({
             }
 
             function hideChildren(e) {
-                //console.log(e);
                 e.preventDefault();
                 e.stopPropagation();
-                //console.log(element.find(this).attr("data-chidden"));
 
                 var children = element.find(this).children();
-                //console.log(children);
                 toogleHide(children);
-
                 //console.log(children);
                 //console.log(element.find(this).data("childrenhidden"));
 
@@ -122,7 +105,6 @@ ccm.component({
 
             };
 
-
             function decideContent(parent, content, flag) {
                 Object.getOwnPropertyNames(content).forEach(function (item) {
                     if (item === "Folder") {
@@ -136,8 +118,6 @@ ccm.component({
 
             function displayFolder(parent, currentFolder, flag) {
                 function handleDropEvent(event, ui) {
-                    //console.log(event);
-                    //console.log(ui);
                     var draggable = ui.draggable;
                     draggable.insertAfter($('#' + event.target.id).children()[0]);
                 }
@@ -148,22 +128,14 @@ ccm.component({
                     var newImage = JSON.parse(JSON.stringify(imageFolder));
                     var newConnectorImage = JSON.parse(JSON.stringify(imageFolder));
                     var lastFolderFlag = false;
-                    //console.log(ccm.helper.find(that, '#' + parent).children.length);
-                    //console.log("parent: " + parent)
                     var level = (parent.split("_").length - 1);
-                    //console.log(parent);
-                    //console.log(level);
                     var lvl = 20;
                     if (flag) {
-                        //if(!veryLastFolder){
                             newFolder.style = "padding-left : " + lvl +"px";
-                        //}
                         veryLastFolder = true;
-
                         level--;
                     }
                     Object.getOwnPropertyNames(fldr).forEach(function (node, ind, root) {
-
                         if (node === "name") {
                             newFolder.inner = fldr[node];
                             newFolder.id = parent + "_" + foldercount;
@@ -181,10 +153,6 @@ ccm.component({
                                     level--;
                                 }
                             }
-
-
-                            //console.log(level);
-                            //console.log(newFolder.inner);
                             if(parent === "node")
                             {
                                 lvl = 0;
@@ -192,19 +160,10 @@ ccm.component({
                             else {
                                 lvl =  20;
                             }
-
                             newFolder.style = "padding-left : " + lvl +"px";
                             ccm.helper.find(that, '#' + parent).append(ccm.helper.html(newFolder));
-                            //for (var i = 0; i < level; i++) {
-                            //    var newImage3 = JSON.parse(JSON.stringify(imageFolder));
-                            //    newImage3.src = "c_bridge.png";
-                            //    newImage3.class = "connector";
-                            //    ccm.helper.find(that, '#' + newFolder.id).append(ccm.helper.html(newImage3));
-                            //}
-
                             ccm.helper.find(that, '#' + newFolder.id).append(ccm.helper.html(newConnectorImage));
                             ccm.helper.find(that, '#' + newFolder.id).append(ccm.helper.html(newImage));
-
                             element.find('div#' + newFolder.id).on('click', hideChildren);
                             element.find('div#' + newFolder.id).droppable({
                                 drop: handleDropEvent
@@ -247,10 +206,7 @@ ccm.component({
             function displayFile(parent, filesArray, flag) {
                 var filecount = 0;
                 var level = (parent.split("_").length - 1);
-                //console.log(filesArray.length);
                 var lastFolderFlag = false;
-
-
                 filesArray.forEach(function (file) {
                     var newFile = JSON.parse(JSON.stringify(file));
                     var newRef = JSON.parse(JSON.stringify(fileref));
@@ -268,9 +224,6 @@ ccm.component({
                         level = level - 1;
                     }
 
-                    //console.log(ccm.helper.find(that, '#' + parent).children.length);
-                    //console.log(newFile.id);
-                    //console.log(filecount);
                     if (filecount < filesArray.length) {
                         newConnectorImage.src = "c_middle.png";
                     }
@@ -278,12 +231,8 @@ ccm.component({
                         newConnectorImage.src = "c_last.png";
                     }
 
-                    //var lvl = "20px";
-                    //newFile.style = "padding-left : " + lvl;
                     lvl =  20;
                     newFile.style = "padding-left : " + lvl +"px";
-
-
                     ccm.helper.find(that, '#' + parent).append(ccm.helper.html(newFile));
                     var FolderElement = ccm.helper.find(that, '#' + newFile.id);
                     //element.find('div#' + newFile.id).draggable( {
@@ -296,7 +245,6 @@ ccm.component({
                     //    newImage3.src = "c_bridge.png";
                     //    ccm.helper.find(that, '#' + newFile.id).append(ccm.helper.html(newImage3));
                     //}
-
                     element.find('div#' + newFile.id).click(function (e) {
                         e.stopPropagation();
                     });
